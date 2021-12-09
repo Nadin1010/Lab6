b49584d47c98ea9cb412cd6355d0b13607e9eac0
Галина <@->
2021-12-09 10:24:56 +0300

    Добавьте файлы проекта.

---
+++ Lab6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Подлинно разумное обучение изменит и наш ум и наши нравы";
+            string wordWithMaxLength = string.Empty;
+            string[] words = s.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+                if (words[i].Length > wordWithMaxLength.Length)
+                    wordWithMaxLength = words[i];
+            Console.WriteLine(wordWithMaxLength);
+            Console.ReadKey();
+        }
+    }
+}
+
